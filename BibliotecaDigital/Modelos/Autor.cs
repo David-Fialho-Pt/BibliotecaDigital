088839ce4208acfa9c40f688334b3297c60d5283
David <davidfialho40@gmail.com>
2026-01-09 08:58:08 +0000

    adicionei modelo de livro e autor ( sujeito a mudança))

---
+++ BibliotecaDigital/Modelos/Autor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca_Digital.Modelos
+{
+    public class Autor
+    {
+        public int IdAutor { get; set; }
+        public string Nome { get; set; }
+        public string Nacionalidade { get; set; }
+        public int AnoNascimento { get; set; }
+    }
+
+}
---
+++ BibliotecaDigital/Modelos/Livro.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca_Digital.Modelos
+{
+   
+        public class Livro
+        {
+            public int IdLivro { get; set; }
+            public string Titulo { get; set; }
+            public int AnoPublicacao { get; set; }
+            public string Editora { get; set; }
+            public string ISBN { get; set; }
+        
+
+       }
+}
